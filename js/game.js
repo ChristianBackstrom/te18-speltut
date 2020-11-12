eaa6d37839de0bcd28b03e6b51b655679b3bbd34
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2020-11-12 09:32:53 +0100

    added assets and made simple movement with img

---
+++ js/game.js
@@ -0,0 +1,13 @@
+var config = {
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade :{
+            gravity: {y:200}
+        }
+    },
+    scene: [Example1]
+};
+
+var game = new Phaser.Game(config);
